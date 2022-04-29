d75448d4e8bddd3789ddcffb794372fddc66b280
patrickcohenn <47466261+patrickcohenn@users.noreply.github.com>
2022-04-28 22:14:37 -0300

    Update Untitled-1.js
    Future home page.

---
+++ Untitled-1.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  .hero-image {
+    background-image: url("https://www.huymai.fi/img/flappybird-go/03.png");
+    background-color: #cccccc;
+    height: 500px;
+    background-position: 80% 62%;
+    background-repeat: no-repeat;
+    background-size: auto;
+    position: relative;
+  }
+
+  .hero-text {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: AliceBlue;
+  }
+  </style>
+  </head>
+  <body>
+      <div class="hero-image">
+        <div class="hero-text">
+          <h1 style="font-size:50px">Meu nome e Patrick</h1>
+          <h3>Sou programador</h3>
+          <button>Click Aqui</button>
+        </div>
+      </div>
+  </body>
+</html>
